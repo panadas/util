c893a01d37399f03298780ee4011506b0f86bf7e
Adam Pancutt <adam@panadas.co.uk>
2014-01-28 14:55:07 +0000

    Whitespace fix

--- tests/PhpTest.php
+++ tests/PhpTest.php
@@ -19,15 +19,15 @@ class PhpTest extends \PHPUnit_Framework_TestCase
     public function toStringProvider()
     {
         return [
-	        [true, "true"],
-	        [false, "false"],
-	        ["foo", "\"foo\""],
-	        [123, 123],
-	        [1.23, 1.23],
-	        [["foo", "bar"], "array(2)"],
-	        [fopen(__FILE__, "r"), "resource(stream)"],
-	        [new \stdClass(), "object(stdClass)"],
-	        [null, "null"]
+            [true, "true"],
+            [false, "false"],
+            ["foo", "\"foo\""],
+            [123, 123],
+            [1.23, 1.23],
+            [["foo", "bar"], "array(2)"],
+            [fopen(__FILE__, "r"), "resource(stream)"],
+            [new \stdClass(), "object(stdClass)"],
+            [null, "null"]
         ];
     }
 
